8850a06fe342fbca38c77d5f9d5fd6193cacb8c8
Mate-20 <78438651+Mate-20@users.noreply.github.com>
2021-12-30 19:41:52 +0530

    Create medicines.sql

---
+++ database/pharmacy_1/medicines.sql
@@ -0,0 +1,17 @@
+-- created table medicines
+
+CREATE TABLE `medicines`(
+`id` int NOT NULL AUTO_INCREMENT ,
+PRIMARY KEY (`id`),
+ `name_medicine` varchar(30), 
+ `price` int
+ );
+
+-- test records added 
+
+INSERT INTO medicines VALUES (1, "Paracetamol", 30);
+INSERT INTO medicines VALUES (2, "Lcz", 25);        
+INSERT INTO medicines VALUES (3, "Coronil", 200);
+INSERT INTO medicines VALUES (4, "Cetirizine", 50);
+
+-- test records ended
